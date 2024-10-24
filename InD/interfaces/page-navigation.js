2916653839d539c48cd70830d663d707507ccfd0
qiewi <105619444+qiewi@users.noreply.github.com>
2024-10-24 14:50:08 +0700

    oops i forgot to delete that js

--- InD/interfaces/page-navigation.js
+++
@@ -1,6 +0,0 @@
-function navigateWithFade(url) {
-    document.body.classList.add('fade-out');  // Add the fade-out class
-    setTimeout(function() {
-        window.location.href = url;  // Navigate after the fade-out
-    }, 500);  // Delay for the duration of the fade-out (500ms)
-}
